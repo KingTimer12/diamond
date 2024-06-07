3c7ff7762798e059923b637f162a8bc2a363d903
kingtimer12 <queentimer12@gmail.com>
2024-06-07 13:23:53 -0300

    fix: corrigido problema de rotas;

--- example/index.js
+++ example/index.js
@@ -1,11 +1,14 @@
 import { start, routes, setupBase } from "diamond"
 import { BaseController } from "./controller/index.js"
-// import CustomBaseRoute from "./routes/base.js"
+import CustomBaseRoute from "./routes/base.js"
 
 setupBase({
     controller() {
         return BaseController
     },
+    route() {
+        return CustomBaseRoute
+    }
 })
 routes({ managerFile: 'example/routes' })
 start({ port: 3030 })
--- example/routes/base.js
+++ example/routes/base.js
@@ -3,8 +3,16 @@ import { AbstractBaseRoute } from "diamond"
 export default class CustomBaseRoute extends AbstractBaseRoute {
     setup(controller, secutiry) {
         const { fetchAll, fetchOne, create, update, destroy } = new controller()
-        this.server.get("/all", fetchAll)
+
+        // Qual rota que tiver /alguma_coisa e nela houver também /:id, deverá colocar o /:id acima.
+        // Senão, o Fastify entenderá que o /alguma_coisa é um id e não outra rota.
+        // Errado:
+        // this.server.get("/all", fetchAll)
+        // this.server.get("/:id", fetchOne)
+        // Certo:
         this.server.get("/:id", fetchOne)
+        this.server.get("/all", fetchAll)
+
         this.server.post("/", create)
         this.server.put("/:id", update)
         this.server.delete("/:id", destroy)
